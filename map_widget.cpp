9e1c4a35be15777c9618c21518b2a793e2f0a07f
Louis Moureaux <m_louis30@yahoo.com>
2021-11-28 23:24:41 +0100

    Add stub GL functions

--- map_widget.cpp
+++ map_widget.cpp
@@ -4,3 +4,23 @@ map_widget::map_widget(QWidget *parent) : QOpenGLWidget(parent)
 {
 
 }
+
+void map_widget::initializeGL()
+{
+    // This function gets called before rendering starts. Any initialization of
+    // the OpenGL context should be done here.
+}
+
+void map_widget::paintGL()
+{
+    // This function implements the main rendering logic.
+}
+
+void map_widget::resizeGL(int w, int h)
+{
+    // This function is called whenever the widget is resized, including on
+    // startup.
+
+    Q_UNUSED(w);
+    Q_UNUSED(h);
+}
--- map_widget.h
+++ map_widget.h
@@ -8,6 +8,11 @@ class map_widget : public QOpenGLWidget
     Q_OBJECT
 public:
     explicit map_widget(QWidget *parent = nullptr);
+
+protected:
+    void initializeGL() override;
+    void paintGL() override;
+    void resizeGL(int w, int h) override;
 };
 
 #endif // MAP_WIDGET_H
